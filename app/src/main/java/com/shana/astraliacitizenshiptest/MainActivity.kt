95f1915c0e4e995d37c58bafbc9c645f3f7f7aad
shanaHirani <shana.hirani@gmail.com>
2022-05-12 22:24:45 +1000

    bottom navigation view

--- app/src/main/java/com/shana/astraliacitizenshiptest/MainActivity.kt
+++ app/src/main/java/com/shana/astraliacitizenshiptest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.shana.astraliacitizenshiptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.myNavHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
